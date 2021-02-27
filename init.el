131425cece943b591ff5ffb7d2933a7757dafa82
Tru <tru.zhou@gmail.com>
2021-02-28 05:55:38 +0800

    passwordstore

--- init.el
+++ init.el
@@ -97,7 +97,8 @@
        macos             ; MacOS-specific commands
        magit             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
-       ;;pass              ; password manager for nerds
+       (pass              ; password manager for nerds
+        +auth)
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
