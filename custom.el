8888b36e765341f31748c25162a082595b38c79d
Tru <tru.zhou@gmail.com>
2021-07-23 03:36:24 +0800

    custom.el

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((eval add-hook 'org-mode-hook #'org-make-toc-mode)
+     (eval add-hook 'after-save-hook
+           (lambda nil
+             (org-babel-tangle))
+           nil t)
+     (eval add-hook 'after-save-hook
+           (lambda nil
+             (if
+                 (y-or-n-p "Tangle?")
+                 (org-babel-tangle)))
+           nil t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
